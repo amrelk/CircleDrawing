3a5c4008e50d45c6e75d92add2ded53609d1c9e6
mmarkle <malcolmmaude01@gmail.com>
2018-04-09 23:59:51 -0500

    made it gooder

--- src/CircleDrawing/CircleDraw.java
+++ src/CircleDrawing/CircleDraw.java
@@ -8,7 +8,7 @@ import processing.core.PVector;
 
 public class CircleDraw extends PApplet {
 	public static void main(String[] args) {PApplet.main("CircleDrawing.CircleDraw");}
-	public void settings() {size(1000, 1000);}
+	public void settings() {fullScreen();/*size(1000, 1000);*/}
 	
 	ArrayList<Circle> circles = new ArrayList<Circle>();
 	Random prg = new Random();
@@ -16,8 +16,8 @@ public class CircleDraw extends PApplet {
 	public void setup() {
 		background(255);
 		
-		circles.add(new Circle(this, new PVector(500, 500), 20));
-		circles.add(new Circle(this, new PVector(470, 500), 10));
+		circles.add(new Circle(this, new PVector(width/2, height/2), 20));
+		circles.add(new Circle(this, new PVector(width/2-30, height/2), 10));
 		for(int i = 0; i < circles.size(); i++) {
 			circles.get(i).draw();
 			circles.get(i).findAdjacents(circles.toArray(new Circle[circles.size()]));
@@ -30,15 +30,15 @@ public class CircleDraw extends PApplet {
 	public void draw() {
 		circles.add(newCircle(circles.get(circles.size()-2), circles.get(circles.size()-1)));
 		circles.get(circles.size()-1).draw();
-		delay(50);
+		//delay(5);
 	}
 	
 	public Circle newCircle(Circle a, Circle b) {
 		Circle result;
 		PVector center = new PVector();
 		float anglecompensation = degrees(PVector.sub(a.center, b.center).heading());
-		float radius = ((a.radius + b.radius) / 2 + (float)prg.nextGaussian());
-		radius = Math.max(Math.min(radius, 500), 5);
+		float radius = ((a.radius + b.radius) / 2 + (float)(prg.nextGaussian()*2-.3));
+		radius = Math.max(Math.min(radius, 100), 5);
 		float angle = acos((pow(a.radius + b.radius,2) - pow(b.radius + radius,2) + pow(a.radius + radius,2))/(2*(a.radius + b.radius)*(a.radius + radius))) + PVector.sub(a.center, b.center).heading();
 		center.x = -(radius + a.radius) * cos(angle) + a.center.x;
 		center.y = -(radius + a.radius) * sin(angle) + a.center.y;
@@ -48,10 +48,14 @@ public class CircleDraw extends PApplet {
 			anglecompensation = degrees(angle);
 			result.center.x = -(radius + a.radius) * cos(angle) + a.center.x;
 			result.center.y = -(radius + a.radius) * sin(angle) + a.center.y;
-			while (result.testOverlap(circles.toArray(new Circle[circles.size()])) && angle < 10) {
-				angle += 0.05;
+			while (result.testOverlap(circles.toArray(new Circle[circles.size()])) && radius > 0.1) {
+				angle += 0.005;
 				result.center.x = -(radius + a.radius) * cos(angle) + a.center.x;
 				result.center.y = -(radius + a.radius) * sin(angle) + a.center.y;
+				if (angle > 10 + prg.nextFloat() * TWO_PI) {
+					radius -= 0.5;
+					angle = 0;
+				}
 			}
 		}
 		return result;
